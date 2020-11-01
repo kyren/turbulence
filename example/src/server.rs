dee3f49233d26e266378868e894f0c062b2a67db
Cedric Hutchings <cedhut02@gmail.com>
2020-11-01 17:49:16 -0500

    Put new clients into a Vec

--- example/src/server.rs
+++ example/src/server.rs
@@ -1,49 +1,47 @@
+use std::sync::mpsc::{sync_channel, SyncSender};
 use comn::Chat;
 use turbulence::MessageChannels;
 
-async fn handle_client(mut channel: MessageChannels) {
-    // Note that we use the async versions of the MessageChannels messages here,
-    // because each client gets their own task on the server, so we don't mind blocking.
-    // On the client, we used the sync versions, because not blocking was desirable.
+fn main() {
+    // Note that some of these clients may very well have long ago disconnected. In order to determine
+    // whether or not a connection is live, you will have to implement some sort of heartbeat system.
+    let mut clients = Vec::with_capacity(100);
+    let (client_tx, client_rx) = sync_channel(100);
+
+    smol::spawn(open_socket(comn::SERVER, 2500, client_tx)).detach();
+
     loop {
-        if let Ok(Chat(msg)) = channel.async_recv().await {
-            println!("server got a message: {}", msg);
+        // Add any new clients to our collection of channels
+        if let Ok(channel) = client_rx.try_recv() {
+            clients.push(channel);
+        }
 
-            // Messages that start with `I'm` get special treatment,
-            // otherwise we just send their message back to them.
-            let response = if msg.len() > 4 && &msg[..4] == "I'm " {
-                format!("Hi {}, I'm server!", &msg[4..])
-            } else {
-                msg
-            };
+        // Check for chat messages from each client
+        for channel in &mut clients {
+            if let Some(Chat(msg)) = channel.recv() {
+                println!("server got a message: {}", msg);
 
-            channel.async_send(Chat(response)).await.unwrap();
+                // Messages that start with `I'm` get special treatment,
+                // otherwise we just send their message back to them.
+                let response = if msg.len() > 4 && &msg[..4] == "I'm " {
+                    format!("Hi {}, I'm server!", &msg[4..])
+                } else {
+                    msg
+                };
+
+                // if .recv() didn't require &mut, I could loop over all the channels
+                // again here and send the message to everyone. Instead, I'll just 
+                // reply only to the person who sent the message.
+                if let Some(Chat(rejected)) = channel.send(Chat(response)) {
+                    println!("couldn't send chat message: {}", rejected)
+                }
+            }
         }
     }
 }
 
-fn main() {
-    // Note that connections here is not a tally of live connections, just a count of total
-    // lifetime connections. In order to determine whether or not a connection is live, you will
-    // have to implement some sort of heartbeat system.
-    let mut connections = 0;
-
-    smol::block_on(open_socket(comn::SERVER, 2500, move |channel| {
-        connections += 1;
-        println!("connections: {}", connections);
-
-        // It's important that processing the messages from the client happens in another task,
-        // so that this one is freed up to keep accepting more clients.
-        smol::spawn(handle_client(channel)).detach();
-    }));
-}
-
 // A UDP socket that accepts new connections for as long as it's open.
-async fn open_socket(
-    my_addr: &str,
-    pool_size: usize,
-    mut channel_handler: impl FnMut(MessageChannels) + Send + 'static,
-) {
+async fn open_socket(my_addr: &str, pool_size: usize, client_tx: SyncSender<MessageChannels>) {
     use comn::{channel_with_multiplexer, send_outgoing_to_socket, SimpleBufferPool};
     use std::collections::HashMap;
     use turbulence::{BufferPacketPool, Packet};
@@ -63,7 +61,7 @@ async fn open_socket(
                     let (channel, multiplexer) = channel_with_multiplexer(pool.clone());
                     let (incoming, outgoing) = multiplexer.start();
                     send_outgoing_to_socket(outgoing, socket.clone(), addr);
-                    channel_handler(channel);
+                    client_tx.send(channel).unwrap();
                     incoming
                 });
                 packet.truncate(len);
